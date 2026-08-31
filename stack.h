f4cab65a5ceec0873a44076bcffae8500f77447f
손우승 <thsdntmd6547@gmail.com>
2026-08-31 20:06:40 +0900

    이제서야 포인터의 기본을 인터넷에서 찾아서 수정중

--- stack.h
+++ stack.h
@@ -5,21 +5,12 @@ typedef struct Stack{
 } Stack;
 
 void push(Stack s, int data) {
-    s.top = (int *)malloc(sizeof(int));
     s.top = &data;
     s.top = s.top++;
 }
 
 int pop(Stack s) {
-    if (s.top == NULL) {
-        return -1;
-    }
-
     int data = *(s.top);
     s.top = s.top--;
     return data;
-}
-
-void del(Stack s) {
-    
 }
