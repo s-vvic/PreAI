f6bb692b34fb01f5815fd12a4dfe3777d195b5ed
손우승 <thsdntmd6547@gmail.com>
2026-09-01 10:42:12 +0900

    AI 없이 많은 수정 끝에 드디어 스택 구현 완료

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "stack.h"
 
 int main() {
-    // Stack s;
-    // push(s, 1);
-    // push(s, 2);
-    // int data = pop(s);
-    // printf("%d", data);
-
-    int *ptr;
-    int test = 10;
-    ptr = &test;
-    printf("角力 蔼: %d, 林家 蔼: %x ", *ptr, ptr);
-    printf("促澜 林家: %x", ++ptr);
+    Stack s;
+    init(&s);
+    push(&s, 1);
+    push(&s, 2);
+    int data = pop(&s);
+    printf("%d \n", data);
 }
--- stack.h
+++ stack.h
@@ -1,16 +1,57 @@
 #include <stdlib.h>
 
-typedef struct Stack{
-    int *top;
+typedef struct Node {
+    int data;
+    struct Node *NextNode;
+    struct Node *PrevNode;
+} Node;
+
+typedef struct Stack {
+    Node *top;
+    int count;
 } Stack;
 
-void push(Stack s, int data) {
-    s.top = &data;
-    s.top = s.top++;
+Node *MakeNode(int data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->NextNode = NULL;
+    node->PrevNode = NULL;
+
+    return node;
 }
 
-int pop(Stack s) {
-    int data = *(s.top);
-    s.top = s.top--;
+void init(Stack *s) {
+    Node *InitNode = (Node *)malloc(sizeof(Node));
+    InitNode->data = ' ';
+    InitNode->NextNode = NULL;
+    InitNode->PrevNode = NULL;
+
+    s->top = InitNode;
+    s->count = 0;
+}
+
+void push(Stack *s, int data) {
+    Node *NewNode = MakeNode(data);
+    Node *PrevNode = s->top;
+    
+    PrevNode->NextNode = NewNode;
+    NewNode->PrevNode = PrevNode;
+    s->top = NewNode;
+    s->count++;
+}
+
+int pop(Stack *s) {
+    if (s->count == 0) {
+        printf("Empty Stack");
+        return -1;
+    }
+
+    int data = s->top->data;
+
+    Node *tmp = s->top;
+    s->top = tmp->PrevNode;
+    free(tmp);
+    s->count--;
+
     return data;
 }
